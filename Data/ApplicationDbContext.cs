9cc547ab8365a6a7a9a4667a078935fc2a344a8e
Modernize Studio <studio@modernize.com>
2026-01-08 07:04:17 +0000

    Commit for planId: 58

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleDotNet6App.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
---
+++ Services/IUserService.cs
@@ -0,0 +1,10 @@
+using SampleDotNet6App.Data;
+
+namespace SampleDotNet6App.Services;
+
+public interface IUserService
+{
+    Task<IEnumerable<User>> GetAllUsersAsync();
+    Task<User?> GetUserByIdAsync(int id);
+    Task<User> CreateUserAsync(User user);
+}
